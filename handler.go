6d14827a616c47858d15d0de6fa61654e3531d67
Ricardo Baumann <gemini.richard@gmail.com>
2018-01-05 15:07:15 +0100

    api gateway support

--- handler.go
+++ handler.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+import (
+	"net/http"
 
-func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
+	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net"
+	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net/apigatewayproxy"
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+)
+
+// Handle is the exported handler called by AWS Lambda.
+var Handle apigatewayproxy.Handler
+
+func init() {
+	ln := net.Listen()
+
+	// Amazon API Gateway binary media types are supported out of the box.
+	// If you don't send or receive binary data, you can safely set it to nil.
+	Handle = apigatewayproxy.New(ln, []string{"image/png"}).Handle
+
+	// Any Go framework complying with the Go http.Handler interface can be used.
+	// This includes, but is not limited to, Vanilla Go, Gin, Echo, Gorrila, Goa, etc.
+	go http.Serve(ln, http.HandlerFunc(handle))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func HandlePlain(evt interface{}, ctx *runtime.Context) (string, error) {
 	return "Hello, World!", nil
 }
