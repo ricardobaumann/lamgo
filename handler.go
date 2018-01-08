7df45ad4f6597942b14d0d2027788fec8dfc65c7
Ricardo Baumann <gemini.richard@gmail.com>
2018-01-08 16:20:32 +0100

    wip

--- handler.go
+++ handler.go
@@ -6,12 +6,16 @@ import (
 	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net"
 	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net/apigatewayproxy"
 	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+	"github.com/go-redis/redis"
 	"io/ioutil"
 	"os"
 	"strings"
 	"io"
 	"log"
+	"fmt"
+	"encoding/json"
 )
+const redisEndpoint = "localhost:6379"
 
 // Handle is the exported handler called by AWS Lambda.
 var Handle apigatewayproxy.Handler
@@ -28,13 +32,30 @@ func init() {
 	go http.Serve(ln, http.HandlerFunc(handle))
 }
 
+
 func handle(w http.ResponseWriter, r *http.Request) {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisEndpoint,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
 	switch r.Method {
 	case "GET":
 		id := strings.TrimPrefix(r.URL.Path, "/test/")
 		w.Write([]byte("Hello, World! from api for id "+id))
 	case "PUT":
 		body, _ := ioutil.ReadAll(r.Body)
+		results := make(map[string][]string)
+		json.Unmarshal(body, &results)
+		for k,v := range results {
+			fmt.Printf("key[%s] value[%s]\n", k, v)
+		}
+
 		w.Write([]byte(string(body)))
 	}
 
